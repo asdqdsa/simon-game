f78f390fbf4f1555b5c2fa0fcac07074f91fb636
asdqdsa <asdqdsa@protonmail.com>
2025-01-11 23:48:00 +0400

    build: update rollup output vite config

--- simon-says/vite.config.js
+++ simon-says/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
+import path from 'path';
 
 export default defineConfig({
   base: '/asdqdsa-JSFE2024Q4/simon-says/',
@@ -12,6 +13,12 @@ export default defineConfig({
       input: {
         main: 'index.html',
         normalize: 'src/styles/modern-normalize.css',
+        app: path.resolve(__dirname, 'src/scripts/app.js'),
+        view: path.resolve(__dirname, 'src/scripts/view.js'),
+        store: path.resolve(__dirname, 'src/scripts/store.js'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js', // Preserve file names
       },
     },
     cssCodeSplit: true, // Enables CSS code splitting
